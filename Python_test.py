15199079c78a5a6e7f85a32328ef46aa1d21c09d
Minguinho-zeze <50979281+Minguinho-zeze@users.noreply.github.com>
2020-09-03 23:09:11 +0900

    코딩 면허 시험 진행 중
    정규 표현식에서 막히고 있다. 어렵다. 파이썬을 잘하려는 길은 참으로 험난한거 같다. 그래도 파이팅.

--- Python_test.py
+++ Python_test.py
@@ -66,17 +66,97 @@
 #         count+=1
 # print(str)
 #문제9(평균값 구하기)
-input_file = open("/Users/minkyu/Documents/PythonCode/sample.txt", 'r')
-nums = []
-while True:
-    num_str = input_file.readline()
-    if not num_str : break
-    nums.append(int(num_str))
-count = 0
-sum = 0
-for num in nums:
-    sum += num
-    count +=1
-avr = sum / count
-print(avr)
-#문제10(사칙연산 계산기)
+# input_file = open("/Users/minkyu/Documents/PythonCode/sample.txt", 'r')
+# nums = []
+# while True:
+#     num_str = input_file.readline()
+#     if not num_str : break
+#     nums.append(int(num_str))
+# count = 0
+# sum = 0
+# for num in nums:
+#     sum += num
+#     count +=1
+# avr = sum / count
+# print(avr)
+#문제10(사칙연산 계산기 클래스) -> 클래스 선언법 복습하기
+# class Calculator:
+#     def __init__(self, list_num):
+#         self.list_num = list_num# 초기화 함수에서 .사용으로 자연스럽게 클래스 변수 선언 + 초기화
+#     def sum(self):
+#         val = 0
+#         for num in self.list_num:
+#             val += num
+#         return val
+
+#     def avg(self):
+#         val = 0
+#         count = 0
+#         for num in self.list_num:
+#             val += num
+#             count +=1
+#         avg = val / count
+#         return avg
+
+# cal1 = Calculator([1,2,3,4,5])
+# print(cal1.sum())
+# print(cal1.avg())
+
+# cal2 = Calculator([6,7,8,9,10])
+# print(cal2.sum())
+# print(cal2.avg())
+
+#문제11(모듈 사용 방법)
+#import mymod를 터미널에서 열어사용할 수 있는 방법
+#1. mymod.py파일 생성 -> 환경변수 path에 mymod.py가 존재하는 경로 추가
+#1밖에 생각이 안나는데...
+
+#문제12(오류와 예외 처리) -> 복습하기
+# result = 0
+# try:
+#     [1,2,3][3]
+#     "a" + 1
+#     4 / 0
+# except TypeError:
+#     result+=1
+# except ZeroDivisionError:
+#     result+=2
+# except IndexError:
+#     result+=3
+# finally:#예외 발생 여부에 상관없이 실행 -> 복습
+#     result+=4
+# print(result)# 3 + 4 = 7
+
+# 문제13(DashInsert함수) -> 무수히 많은 변수 선언이 필요했다. 분명 변수 선언을 적게 하고도 문제를 푸는 방법이 있지 않을까 싶다.
+# 숫자로 구성된 문자열을 입력받은 뒤 문자열 안에서 홀수가 연속되면 두 수 사이에 - 추가, 짝수가 연속되면 *를 추가.
+# def DashInsert(num):
+#     num_str = str(num)
+#     last_char = ''
+#     result_str = ""
+#     count = 0
+#     for chr in num_str:
+#         if(count != 0):
+#             num_last_char = int(last_char)
+#             num_chr = int(chr)
+
+#             if( num_last_char % 2 == 0 and num_chr % 2 == 0):# 둘다 짝수
+#                 plus_chr = '*'+chr
+#                 result_str +=plus_chr
+#             elif( num_last_char % 2 == 1 and num_chr % 2 == 1 ):#둘다 홀수
+#                 plus_chr = '-'+chr
+#                 result_str += plus_chr
+#             else:
+#                 result_str +=chr#아무것도 아님
+#         else:
+#             result_str = chr
+#         last_char = chr
+#         count+=1
+#     return result_str
+
+# print(DashInsert(4546793))
+#문제14(문자열 압축) -> 복습
+
+
+                
+
+
